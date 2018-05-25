fb282d46d0f7e69ad853affe3171ead43b20e50d
Anton Yakushev <yakushevanton@gmail.com>
2018-05-25 19:19:03 +0400

    Added editorconfig

--- Arabidopsis/routes.py
+++ Arabidopsis/routes.py
@@ -6,24 +6,24 @@ app = Flask(__name__)
 app.secret_key = 'development key'
 
 class RunnerForm(Form):
-  filename = StringField("filename")
-  submit = SubmitField("send")
+    filename = StringField("filename")
+    submit = SubmitField("send")
 
 @app.route('/form', methods=['GET', 'POST'])
 def runner():
-  form = RunnerForm()
- 
-  if request.method == 'POST':
-    try:
-      command = 'python ./' + request.form["filename"] + ' ./data/TAIR10_GFF3_genes.gff'
-      # Security hole, do not do this in real life
-      retcode = subprocess.call(command, shell=True)
-      if retcode < 0:
-        return "Excution was terminated by signal"
-      else:
-        return "Excuted script"
-    except OSError as e:
-      return "Execution failed " + request.form["filename"] + " " + str(e)
- 
-  elif request.method == 'GET':
-    return render_template('form.html', form=form)
+    form = RunnerForm()
+
+    if request.method == 'POST':
+        try:
+            command = 'python ./' + request.form["filename"] + ' ./data/TAIR10_GFF3_genes.gff'
+            # Security hole, do not do this in real life
+            retcode = subprocess.call(command, shell=True)
+            if retcode < 0:
+                return "Excution was terminated by signal"
+            else:
+                return "Excuted script"
+        except OSError as e:
+            return "Execution failed " + request.form["filename"] + " " + str(e)
+
+    elif request.method == 'GET':
+        return render_template('form.html', form=form)
